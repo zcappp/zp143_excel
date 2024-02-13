8bfa11b6cb4cb6a08904a30a0bdf7f36122db952
众触应用平台 <87555332+zcappp@users.noreply.github.com>
2024-02-13 08:49:07 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: process.env.mode || "development",
     devtool: false,
     entry: {
-        "zp143": "./plugins/zp143_电子表格.js",
+        "zp143": "./src/zp143_电子表格.js",
     },
     output: {
         path: path.join(__dirname, "build/"),
